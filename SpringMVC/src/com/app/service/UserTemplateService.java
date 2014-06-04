b2c64a35c5083009d2f7f7445f2f7d094ecdde3e
gwq <aligwqhz@gmail.com>
2014-06-04 16:58:07 +0800

    HitbernateDaoSupport

--- SpringMVC/src/com/app/service/UserTemplateService.java
+++ SpringMVC/src/com/app/service/UserTemplateService.java
@@ -1,9 +1,7 @@
 package com.app.service;
 
-import org.hibernate.Query;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.orm.hibernate3.HibernateTemplate;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,21 +10,20 @@ import com.app.model.TestModel;
 
 @Service
 @Transactional
-public class UserService{
+public class UserTemplateService  extends HibernateDaoSupport{
 	
 	@Autowired
-	private SessionFactory sessionFactory;
+	public void setSessionFacotry(SessionFactory sessionFactory){
+		super.setSessionFactory(sessionFactory);
+	}
 	
 	public int userNum(){
-		if (sessionFactory != null){
-			System.out.println("SessionFacotory is NOT NULL!!!");
-		}
-		//Query query = sessionFactory.getCurrentSession().createSQLQuery("INSERT INTO `bo_gefms_testmodel` (`id`,`name`) VALUES (1,'ge')");
-		//query.executeUpdate();
+		
 		TestModel tm = new TestModel();
 		//tm.setId(1);
 		tm.setName("ds");
-		sessionFactory.getCurrentSession().save(tm);
+		this.getHibernateTemplate().save(tm);
 		return 3;
 	}
+
 }
